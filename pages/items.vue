852b85eeba0528e2b3e11843a79ec2859b43a12e
Jaychandmourya <jaychandm@zignuts.com>
2023-02-23 10:33:05 +0530

    return change itempage

--- pages/items.vue
+++ pages/items.vue
@@ -250,14 +250,6 @@ export default {
       }
       this.close();
     },
-    save() {
-      if (this.editedIndex > -1) {
-        Object.assign(this.desserts[this.editedIndex], this.editedItem);
-      } else {
-        this.desserts.push(this.editedItem);
-      }
-      this.close();
-    },
   },
 };
 </script>
